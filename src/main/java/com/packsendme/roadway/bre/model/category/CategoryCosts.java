4da1a3a628dcb48bfabe77864a2a61254d28c044
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-10-06 22:06:35 +0200

    NewVersion/FieldStatus

--- src/main/java/com/packsendme/roadway/bre/model/category/CategoryCosts.java
+++ src/main/java/com/packsendme/roadway/bre/model/category/CategoryCosts.java
@@ -23,10 +23,12 @@ public class CategoryCosts implements Serializable {
 	public Double average_consumption_cost;
 	public Double rate_exchange;
 	public String current_exchange;
+	public Boolean statusChange;
 
 	
 	public CategoryCosts(String countryName, String vehicle, Double weight_cost, Double distance_cost,
-			Double worktime_cost, Double average_consumption_cost, Double rate_exchange, String current_exchange) {
+			Double worktime_cost, Double average_consumption_cost, Double rate_exchange, 
+			String current_exchange, Boolean statusChange) {
 		super();
 		this.countryName = countryName;
 		this.vehicle = vehicle;
@@ -36,6 +38,7 @@ public class CategoryCosts implements Serializable {
 		this.average_consumption_cost = average_consumption_cost;
 		this.rate_exchange = rate_exchange;
 		this.current_exchange = current_exchange;
+		this.statusChange = statusChange;
 	}
 
 
